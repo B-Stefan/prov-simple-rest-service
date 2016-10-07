032870117e2e58a635876cdb93dcac73b767671e
Stefan B <Stefan.bie@live.de>
2016-10-07 17:24:59 +0200

    changed to api.yaml

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ port = int(os.environ.get("PORT", 9090))
 
 app = connexion.App(__name__, port, specification_dir='./')
 
-app.add_api('./app.yaml', arguments={'title': 'Simple Prov-Document rest service'}, resolver=RestyResolver("api"))
+app.add_api('./api.yaml', arguments={'title': 'Simple Prov-Document rest service'}, resolver=RestyResolver("api"))
 
 if __name__ == '__main__':
     app.run(debug=True)
