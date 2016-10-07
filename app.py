82e0407dad06ca15d5c6503a19b6ec278fcc2296
Stefan B <Stefan.bie@live.de>
2016-10-07 17:08:31 +0200

    - only if command line

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from connexion.resolver import RestyResolver
 import os
 port = int(os.environ.get("PORT", 9090))
 
-app = connexion.App(__name__, port, specification_dir='./',host='0.0.0.0')
+app = connexion.App(__name__, port, specification_dir='./')
 
 app.add_api('./example.yaml', arguments={'title': 'Simple Prov-Document rest service'}, resolver=RestyResolver("api"))
 
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
