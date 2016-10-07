b54fc329823d07c645ec2dd019337763312607e9
Stefan B <Stefan.bie@live.de>
2016-10-07 16:38:52 +0200

    - add all heroku changes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -w 4 -b 0.0.0.0:$PORT  example/simple_rest_service:app
+web: gunicorn -w 4 -b 0.0.0.0:$PORT -k gevent example/simple_rest_service:app
--- example/simple_rest_service.py
+++ example/simple_rest_service.py
@@ -7,10 +7,8 @@ prov_api = ProvApi(adapter=SimpleInMemoryAdapter, auth_info=None)
 
 port = int(os.environ.get("PORT", 9090))
 
-print(port)
+app = connexion.App(__name__, port, specification_dir='./',host='0.0.0.0')
+app.add_api('simple_rest_service.yaml', arguments={'title': 'Simple Prov-Document rest service'}, resolver=RestyResolver("api"))
 
 if __name__ == '__main__':
-
-    app = connexion.App(__name__, port, specification_dir='./',host='0.0.0.0')
-    app.add_api('simple_rest_service.yaml', arguments={'title': 'Simple Prov-Document rest service'}, resolver=RestyResolver("api"))
     app.run()
